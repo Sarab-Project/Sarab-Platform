098da5a1dd995ac20e755737d7807e2d49297998
ahmad <ahmad2315753@gmail.com>
2026-04-13 09:28:59 +0300

    move backend files to backend folder

--- backend/obj/Debug/net10.0/Sarab-Platform.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/Sarab-Platform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sarab-Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993475d835396a564cdf4675125bbcaa6413cef3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sarab-Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sarab-Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
