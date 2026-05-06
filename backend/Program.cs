4b8292728c43bd0c6a0687a1e9e191d8f6fc800b
ahmad <ahmad2315753@gmail.com>
2026-05-06 17:20:43 +0300

    Add Program file

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,13 +1,59 @@
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
+using SarabPlatform.Data;
+using SarabPlatform.Services;
+using Sarab_Platform.Seeders;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
 
-builder.Services.AddOpenApi();
+builder.Services.Configure<FormOptions>(options =>
+{
+    options.MultipartBodyLengthLimit = 200 * 1024 * 1024; // 200 MB
+    options.ValueLengthLimit = int.MaxValue;
+    options.MemoryBufferThreshold = int.MaxValue;
+});
 
+builder.Services.AddHttpClient(); 
+builder.Services.AddScoped<FileService>();
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+    options.AddPolicy("AllowAll",
+    policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    })
+);
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
+    await DbSeeder.SeedAsync(db);
+}
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
+app.UseCors("AllowAll");
+app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapGet("/", () => "Root endpoint!");
 
 app.Run();
+
+
