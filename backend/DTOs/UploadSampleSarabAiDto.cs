bfc1da603eb2c21f408be54690f844c57f4d7bde
ahmad <ahmad2315753@gmail.com>
2026-05-06 16:27:26 +0300

    Sarab-ai Upload Api

---
+++ backend/DTOs/UploadSampleSarabAiDto.cs
@@ -0,0 +1,14 @@
+namespace SarabPlatform.Dto
+{
+    public class UploadSampleSarabAiDto
+    {
+        public string? EyeSide { get; set; }
+        public string? Gender { get; set; }
+        public int Age { get; set; }
+        public string? City { get; set; }
+        public string? Status { get; set; }
+        public string? Profession { get; set; }
+        public string? Notes { get; set; }
+        public List<IFormFile> Videos { get; set; }
+    }
+}
