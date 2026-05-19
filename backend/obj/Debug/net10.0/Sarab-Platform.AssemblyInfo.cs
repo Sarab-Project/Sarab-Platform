1977f143dad16edbbf3f1148a296e66795f06d70
ahmad <ahmad2315753@gmail.com>
2026-05-19 22:58:23 +0300

    fix

--- backend/obj/Debug/net10.0/Sarab-Platform.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/Sarab-Platform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sarab-Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e93c7a5eee6fac929bab8e46c3c9e4baab662928")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e03de72eca7e28e8dcb3932b059d7ef7f1ba247")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sarab-Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sarab-Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
